b12a752920ccc54b3f4e47d45c3f3a36fe7a0248
Nico <51355646+nico123469@users.noreply.github.com>
2024-12-13 22:01:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,122 @@
 const capitals = [
-  { name: "London", country: "United Kingdom", timezoneOffset: 0, lat: 51.5074, lon: -0.1278, code: "gb" },
-  { name: "Berlin", country: "Germany", timezoneOffset: 1, lat: 52.52, lon: 13.405, code: "de" },
-  { name: "Paris", country: "France", timezoneOffset: 1, lat: 48.8566, lon: 2.3522, code: "fr" },
-  { name: "Cairo", country: "Egypt", timezoneOffset: 2, lat: 30.0444, lon: 31.2357, code: "eg" },
-  { name: "Moscow", country: "Russia", timezoneOffset: 3, lat: 55.7558, lon: 37.6173, code: "ru" },
-  { name: "Dubai", country: "United Arab Emirates", timezoneOffset: 4, lat: 25.276987, lon: 55.296249, code: "ae" },
-  { name: "New Delhi", country: "India", timezoneOffset: 5.5, lat: 28.6139, lon: 77.2090, code: "in" },
-  { name: "Dhaka", country: "Bangladesh", timezoneOffset: 6, lat: 23.8103, lon: 90.4125, code: "bd" },
-  { name: "Jakarta", country: "Indonesia", timezoneOffset: 7, lat: -6.2088, lon: 106.8456, code: "id" },
-  { name: "Singapore", country: "Singapore", timezoneOffset: 8, lat: 1.3521, lon: 103.8198, code: "sg" },
-  { name: "Beijing", country: "China", timezoneOffset: 8, lat: 39.9042, lon: 116.4074, code: "cn" },
-  { name: "Tokyo", country: "Japan", timezoneOffset: 9, lat: 35.6895, lon: 139.6917, code: "jp" },
-  { name: "Seoul", country: "South Korea", timezoneOffset: 9, lat: 37.5665, lon: 126.9780, code: "kr" },
-  { name: "Canberra", country: "Australia", timezoneOffset: 10, lat: -35.2809, lon: 149.13, code: "au" },
-  { name: "Honolulu", country: "USA", timezoneOffset: -10, lat: 21.3069, lon: -157.8583, code: "us-hi" },
-  { name: "Anchorage", country: "USA", timezoneOffset: -9, lat: 61.2181, lon: -149.9003, code: "us-ak" },
-  { name: "Los Angeles", country: "USA", timezoneOffset: -8, lat: 34.0522, lon: -118.2437, code: "us-ca" },
-  { name: "Denver", country: "USA", timezoneOffset: -7, lat: 39.7392, lon: -104.9903, code: "us-co" },
-  { name: "Mexico City", country: "Mexico", timezoneOffset: -6, lat: 19.4326, lon: -99.1332, code: "mx" },
-  { name: "New York", country: "USA", timezoneOffset: -5, lat: 40.7128, lon: -74.0060, code: "us" },
-  { name: "Brasília", country: "Brazil", timezoneOffset: -3, lat: -15.7801, lon: -47.9292, code: "br" },
-  { name: "Buenos Aires", country: "Argentina", timezoneOffset: -3, lat: -34.6037, lon: -58.3816, code: "ar" }
+{ name: "London", country: "United Kingdom", timezoneOffset: 0, lat: 51.5074, lon: -0.1278, code: "gb" },
+{ name: "Lisbon", country: "Portugal", timezoneOffset: 0, lat: 38.7169, lon: -9.139, code: "pt" },
+{ name: "Dublin", country: "Ireland", timezoneOffset: 0, lat: 53.3498, lon: -6.2603, code: "ie" },
+
+{ name: "Berlin", country: "Germany", timezoneOffset: 1, lat: 52.52, lon: 13.405, code: "de" },
+{ name: "Paris", country: "France", timezoneOffset: 1, lat: 48.8566, lon: 2.3522, code: "fr" },
+{ name: "Madrid", country: "Spain", timezoneOffset: 1, lat: 40.4168, lon: -3.7038, code: "es" },
+
+{ name: "Cairo", country: "Egypt", timezoneOffset: 2, lat: 30.0444, lon: 31.2357, code: "eg" },
+{ name: "Athens", country: "Greece", timezoneOffset: 2, lat: 37.9838, lon: 23.7275, code: "gr" },
+{ name: "Istanbul", country: "Turkey", timezoneOffset: 2, lat: 41.0082, lon: 28.9784, code: "tr" },
+
+{ name: "Moscow", country: "Russia", timezoneOffset: 3, lat: 55.7558, lon: 37.6173, code: "ru" },
+{ name: "Riyadh", country: "Saudi Arabia", timezoneOffset: 3, lat: 24.7136, lon: 46.6753, code: "sa" },
+{ name: "Nairobi", country: "Kenya", timezoneOffset: 3, lat: -1.286389, lon: 36.817223, code: "ke" },
+
+{ name: "Dubai", country: "United Arab Emirates", timezoneOffset: 4, lat: 25.276987, lon: 55.296249, code: "ae" },
+{ name: "Baku", country: "Azerbaijan", timezoneOffset: 4, lat: 40.4093, lon: 49.8671, code: "az" },
+{ name: "Tbilisi", country: "Georgia", timezoneOffset: 4, lat: 41.7151, lon: 44.8271, code: "ge" },
+
+{ name: "Male", country: "Maldives", timezoneOffset: 5, lat: 4.1755, lon: 73.5093, code: "mv" },
+{ name: "Tashkent", country: "Uzbekistan", timezoneOffset: 5, lat: 41.2995, lon: 69.2401, code: "uz" },
+{ name: "Ashgabat", country: "Turkmenistan", timezoneOffset: 5, lat: 37.9601, lon: 58.3261, code: "tm" },
+{ name: "Karachi", country: "Pakistan", timezoneOffset: 5, lat: 24.8607, lon: 67.0011, code: "pk" },
+{ name: "Samarkand", country: "Uzbekistan", timezoneOffset: 5, lat: 39.6542, lon: 66.9597, code: "uz" },
+{ name: "Kuda Bandos", country: "Maldives", timezoneOffset: 5, lat: 4.2456, lon: 73.5414, code: "mv" },
+{ name: "Addu City", country: "Maldives", timezoneOffset: 5, lat: -0.6305, lon: 73.0926, code: "mv" },
+{ name: "Dushanbe", country: "Tajikistan", timezoneOffset: 5, lat: 38.5598, lon: 68.7870, code: "tj" },
+{ name: "Nukus", country: "Uzbekistan", timezoneOffset: 5, lat: 42.4531, lon: 59.6075, code: "uz" },
+{ name: "Khujand", country: "Tajikistan", timezoneOffset: 5, lat: 40.2833, lon: 69.6167, code: "tj" },
+{ name: "Hithadhoo", country: "Maldives", timezoneOffset: 5, lat: -0.6000, lon: 73.0833, code: "mv" },
+
+{ name: "Dhaka", country: "Bangladesh", timezoneOffset: 6, lat: 23.8103, lon: 90.4125, code: "bd" },
+{ name: "Thimphu", country: "Bhutan", timezoneOffset: 6, lat: 27.4728, lon: 89.639, code: "bt" },
+{ name: "Almaty", country: "Kazakhstan", timezoneOffset: 6, lat: 43.222, lon: 76.8512, code: "kz" },
+
+{ name: "Jakarta", country: "Indonesia", timezoneOffset: 7, lat: -6.2088, lon: 106.8456, code: "id" },
+{ name: "Bangkok", country: "Thailand", timezoneOffset: 7, lat: 13.7563, lon: 100.5018, code: "th" },
+{ name: "Hanoi", country: "Vietnam", timezoneOffset: 7, lat: 21.0285, lon: 105.8542, code: "vn" },
+
+{ name: "Singapore", country: "Singapore", timezoneOffset: 8, lat: 1.3521, lon: 103.8198, code: "sg" },
+{ name: "Beijing", country: "China", timezoneOffset: 8, lat: 39.9042, lon: 116.4074, code: "cn" },
+{ name: "Kuala Lumpur", country: "Malaysia", timezoneOffset: 8, lat: 3.139, lon: 101.6869, code: "my" },
+
+{ name: "Tokyo", country: "Japan", timezoneOffset: 9, lat: 35.6895, lon: 139.6917, code: "jp" },
+{ name: "Seoul", country: "South Korea", timezoneOffset: 9, lat: 37.5665, lon: 126.9780, code: "kr" },
+{ name: "Pyongyang", country: "North Korea", timezoneOffset: 9, lat: 39.0194, lon: 125.7381, code: "kp" },
+
+{ name: "Canberra", country: "Australia", timezoneOffset: 10, lat: -35.2809, lon: 149.13, code: "au" },
+{ name: "Sydney", country: "Australia", timezoneOffset: 10, lat: -33.8688, lon: 151.2093, code: "au" },
+{ name: "Port Moresby", country: "Papua New Guinea", timezoneOffset: 10, lat: -9.4438, lon: 147.1803, code: "pg" },
+
+  { name: "Noumea", country: "New Caledonia", timezoneOffset: 11, lat: -22.2758, lon: 166.4481, code: "nc" },
+  { name: "Honiara", country: "Solomon Islands", timezoneOffset: 11, lat: -9.433, lon: 159.953, code: "sb" },
+  { name: "Port Vila", country: "Vanuatu", timezoneOffset: 11, lat: -17.7344, lon: 168.3193, code: "vu" },
+
+  { name: "Tarawa", country: "Kiribati", timezoneOffset: 12, lat: 1.4513, lon: 173.0335, code: "ki" },
+  { name: "Funafuti", country: "Tuvalu", timezoneOffset: 12, lat: -7.4816, lon: 179.194, code: "tv" },
+  { name: "Auckland", country: "New Zealand", timezoneOffset: 12, lat: -36.8485, lon: 174.7633, code: "nz" },
+
+  { name: "Pago Pago", country: "American Samoa", timezoneOffset: 13, lat: -14.278, lon: -170.702, code: "as" },
+  { name: "Apia", country: "Samoa", timezoneOffset: 13, lat: -13.8295, lon: -172.1046, code: "ws" },
+  { name: "Nuku'alofa", country: "Tonga", timezoneOffset: 13, lat: -21.1394, lon: -175.2011, code: "to" },
+
+  { name: "Kiritimati", country: "Kiribati", timezoneOffset: 14, lat: 1.875, lon: -157.425, code: "ki" },
+  { name: "Millennium Island", country: "Kiribati", timezoneOffset: 14, lat: 0.697, lon: -151.387, code: "ki" },
+  { name: "Teraina", country: "Kiribati", timezoneOffset: 14, lat: 3.226, lon: -159.972, code: "ki" },
+
+  { name: "Baker Island", country: "United States (unincorporated territory)", timezoneOffset: -12, lat: 0.1936, lon: -176.4824, code: "us" },
+  { name: "Howland Island", country: "United States (unincorporated territory)", timezoneOffset: -12, lat: 0.7833, lon: -176.6167, code: "us" },
+  { name: "Jarvis Island", country: "United States (unincorporated territory)", timezoneOffset: -12, lat: -0.3506, lon: -160.0333, code: "us" },
+
+{ name: "Pago Pago", country: "American Samoa", timezoneOffset: -11, lat: -14.2756, lon: -170.7026, code: "as" },
+{ name: "Niue", country: "Niue", timezoneOffset: -11, lat: -19.0544, lon: -169.8682, code: "nu" },
+{ name: "Midway Atoll", country: "United States", timezoneOffset: -11, lat: 28.2096, lon: -177.3753, code: "us" },
+
+{ name: "Honolulu", country: "USA", timezoneOffset: -10, lat: 21.3069, lon: -157.8583, code: "us-hi" },
+{ name: "Papeete", country: "French Polynesia", timezoneOffset: -10, lat: -17.5516, lon: -149.5585, code: "pf" },
+{ name: "Hilo", country: "USA", timezoneOffset: -10, lat: 19.7297, lon: -155.09, code: "us-hi" },
+
+{ name: "Anchorage", country: "USA", timezoneOffset: -9, lat: 61.2181, lon: -149.9003, code: "us-ak" },
+{ name: "Fairbanks", country: "USA", timezoneOffset: -9, lat: 64.8378, lon: -147.7164, code: "us-ak" },
+{ name: "Juneau", country: "USA", timezoneOffset: -9, lat: 58.3019, lon: -134.4197, code: "us-ak" },
+
+{ name: "Los Angeles", country: "USA", timezoneOffset: -8, lat: 34.0522, lon: -118.2437, code: "us-ca" },
+{ name: "San Francisco", country: "USA", timezoneOffset: -8, lat: 37.7749, lon: -122.4194, code: "us-ca" },
+{ name: "Seattle", country: "USA", timezoneOffset: -8, lat: 47.6062, lon: -122.3321, code: "us-wa" },
+
+{ name: "Denver", country: "USA", timezoneOffset: -7, lat: 39.7392, lon: -104.9903, code: "us-co" },
+{ name: "Salt Lake City", country: "USA", timezoneOffset: -7, lat: 40.7608, lon: -111.891, code: "us-ut" },
+{ name: "Albuquerque", country: "USA", timezoneOffset: -7, lat: 35.0844, lon: -106.6504, code: "us-nm" },
+
+{ name: "Mexico City", country: "Mexico", timezoneOffset: -6, lat: 19.4326, lon: -99.1332, code: "mx" },
+{ name: "Guatemala City", country: "Guatemala", timezoneOffset: -6, lat: 14.6349, lon: -90.5069, code: "gt" },
+{ name: "San Salvador", country: "El Salvador", timezoneOffset: -6, lat: 13.6929, lon: -89.2182, code: "sv" },
+
+{ name: "New York", country: "USA", timezoneOffset: -5, lat: 40.7128, lon: -74.0060, code: "us" },
+{ name: "Toronto", country: "Canada", timezoneOffset: -5, lat: 43.65107, lon: -79.347015, code: "ca" },
+{ name: "Havana", country: "Cuba", timezoneOffset: -5, lat: 23.1136, lon: -82.3666, code: "cu" },
+
+{ name: "Caracas", country: "Venezuela", timezoneOffset: -4, lat: 10.4806, lon: -66.9036, code: "ve" },
+{ name: "La Paz", country: "Bolivia", timezoneOffset: -4, lat: -16.5000, lon: -68.1193, code: "bo" },
+{ name: "Georgetown", country: "Guyana", timezoneOffset: -4, lat: 6.8013, lon: -58.1552, code: "gy" },
+
+{ name: "Brasília", country: "Brazil", timezoneOffset: -3, lat: -15.7801, lon: -47.9292, code: "br" },
+{ name: "Buenos Aires", country: "Argentina", timezoneOffset: -3, lat: -34.6037, lon: -58.3816, code: "ar" },
+{ name: "Montevideo", country: "Uruguay", timezoneOffset: -3, lat: -34.9011, lon: -56.1645, code: "uy" },
+
+{ name: "Fernando de Noronha", country: "Brazil", timezoneOffset: -2, lat: -3.8586, lon: -32.4291, code: "br" },
+{ name: "South Georgia", country: "United Kingdom", timezoneOffset: -2, lat: -54.4296, lon: -36.5870, code: "gb" },
+{ name: "Saint Pierre", country: "Saint Pierre and Miquelon", timezoneOffset: -2, lat: 46.7720, lon: -56.1710, code: "pm" },
+
+{ name: "Azores", country: "Portugal", timezoneOffset: -1, lat: 37.7412, lon: -25.6756, code: "pt" },
+{ name: "Madeira", country: "Portugal", timezoneOffset: -1, lat: 32.7607, lon: -16.9595, code: "pt" },
+{ name: "Cape Verde", country: "Cape Verde", timezoneOffset: -1, lat: 15.1200, lon: -23.6050, code: "cv" }
+
 ];
+
 
 const clockElement = document.getElementById("clock");
 const capitalNameElement = document.getElementById("capital-name");
@@ -34,50 +129,60 @@ let clockInterval;
 
 function updateClock(timezoneOffset) {
   const utcNow = new Date();  // Aktuelle UTC-Zeit
-  const localTime = new Date(utcNow.getTime() + timezoneOffset * 60 * 60 * 1000);  // Berechnung der lokalen Zeit
-
-  const hours = String(localTime.getHours()).padStart(2, "0");
-  const minutes = String(localTime.getMinutes()).padStart(2, "0");
-  const seconds = String(localTime.getSeconds()).padStart(2, "0");
-  clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+  
+  // Zeitzone unter Berücksichtigung von Sommer-/Winterzeit berechnen
+  const formatter = new Intl.DateTimeFormat("en-US", {
+    timeZone: `Etc/GMT${timezoneOffset > 0 ? `-${timezoneOffset}` : `+${Math.abs(timezoneOffset)}`}`,
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  });
+  const parts = formatter.formatToParts(utcNow);
+  const timeParts = {
+    hours: parts.find(part => part.type === "hour").value,
+    minutes: parts.find(part => part.type === "minute").value,
+    seconds: parts.find(part => part.type === "second").value,
+  };
+  
+  clockElement.textContent = `${timeParts.hours}:${timeParts.minutes}:${timeParts.seconds}`;
 }
+
 
 async function loadRandomCapital() {
   let foundCapital = false;
-  let offsetAdjust = 0;
 
   while (!foundCapital) {
-    for (const randomCapital of capitals) {
-      const { name, country, lat, lon, code, timezoneOffset } = randomCapital;
+    const randomCapital = capitals[Math.floor(Math.random() * capitals.length)];
+    const { name, country, lat, lon, code, timezoneOffset } = randomCapital;
 
-      const utcNow = new Date();  // Aktuelle UTC-Zeit
-      const adjustedOffset = timezoneOffset + offsetAdjust;
-      const localTime = new Date(utcNow.getTime() + adjustedOffset * 60 * 60 * 1000);  // Berechnung der lokalen Zeit für die Hauptstadt
+    const utcNow = new Date();
+    
+    // Lokale Zeit unter Berücksichtigung der Sommer-/Winterzeit
+    const formatter = new Intl.DateTimeFormat("en-US", {
+      timeZone: `Etc/GMT${timezoneOffset > 0 ? `-${timezoneOffset}` : `+${Math.abs(timezoneOffset)}`}`,
+      hour: "2-digit",
+      hour12: false,
+    });
+    const localHours = parseInt(formatter.formatToParts(utcNow).find(part => part.type === "hour").value, 10);
 
-      const localHours = localTime.getHours();
+    // Nur Städte zwischen 16:00 und 17:00 Uhr lokalzeit anzeigen
+    if (localHours >= 16 && localHours < 17) {
+      capitalNameElement.textContent = name;
+      countryNameElement.textContent = country;
+      mapImageElement.src = `https://static-maps.yandex.ru/1.x/?ll=${lon},${lat}&z=12&size=600,300&l=map`;
+      mapsLinkElement.href = `https://www.openstreetmap.org/?mlat=${lat}&mlon=${lon}#map=12/${lat}/${lon}`;
+      mapsLinkElement.textContent = `Zur interaktiven Karte von ${name}`;
 
-      // Suche nach der ersten Stadt, die in der gewünschten Zeitzone oder angepassten Zeitzone liegt
-      if (localHours >= 16 && localHours < 17) {
-        capitalNameElement.textContent = name;
-        countryNameElement.textContent = country;
-        mapImageElement.src = `https://static-maps.yandex.ru/1.x/?ll=${lon},${lat}&z=12&size=600,300&l=map`;
-        mapsLinkElement.href = `https://www.openstreetmap.org/?mlat=${lat}&mlon=${lon}#map=12/${lat}/${lon}`;
-        mapsLinkElement.textContent = `Zur interaktiven Karte von ${name}`;
+      // Uhrzeit aktualisieren
+      if (clockInterval) clearInterval(clockInterval); // Vorheriges Intervall stoppen
+      clockInterval = setInterval(() => updateClock(timezoneOffset), 1000);
 
-        // Uhrzeit aktualisieren
-        if (clockInterval) clearInterval(clockInterval); // Vorheriges Intervall stoppen
-        clockInterval = setInterval(() => updateClock(timezoneOffset), 1000);
-
-        foundCapital = true;  // Passende Stadt gefunden
-        break;
-      }
-    }
-
-    if (!foundCapital) {
-      offsetAdjust += 1; // Eine Stunde weiter suchen
+      foundCapital = true;  // Passende Stadt gefunden
     }
   }
 }
+
 
 // Event listener für den Button
 reloadButton.addEventListener("click", loadRandomCapital);
